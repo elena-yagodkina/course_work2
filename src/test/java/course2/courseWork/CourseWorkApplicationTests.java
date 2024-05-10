b3cfd81df92858efdc363d8d2993c5482e01f132
elena_yagodkina <engineerlena@mail.ru>
2024-05-10 08:37:00 +0300

    push_master

---
+++ src/test/java/course2/courseWork/CourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package course2.courseWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
